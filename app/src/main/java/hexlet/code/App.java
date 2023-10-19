cff196405e37bcceb667c5ab9f1fbf5819ee0e16
Timur <kta.com@bk.ru>
2023-10-19 15:10:51 +0300

    update App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,40 +3,49 @@ package hexlet.code;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
-
 import picocli.CommandLine.Parameters;
+
+import java.io.IOException;
 import java.util.concurrent.Callable;
+
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
-public class App implements Callable<Integer>{
-    @Option(names = { "-f", "--format" },
-            paramLabel = "format",
-            defaultValue = "stylish",
-            description = "output format [default: ${DEFAULT-VALUE}]")
-    private String format;
-    @Parameters(index = "0", description = "path to first file")
-    private String filePath1;
-    @Parameters(index = "1", description = "path to second file")
-    private String filePath2;
-    public static void main(String[] args) {
-        int exitCode = new CommandLine(new App()).execute(args);
-        System.exit(exitCode);
-    }
 
+public class App implements Callable<Integer> {
+    @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
+    private static String filepath1;
+    @Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
+    private static String filepath2;
+    @Option(names = {"-f", "--format"},
+            description = "output format [default: stylish]",
+            paramLabel = "format")
+    private String format;
+    @Option(names = {"-h", "--help"}, usageHelp = true,
+            description = "Show this help message and exit.")
+    private boolean helpRequested;
+    @Option(names = {"V", "--version"}, versionHelp = true,
+            description = "Print version information and exit.")
+    private boolean versionRequested;
     @Override
     public Integer call() throws Exception {
         try {
-            System.out.println("hello");
-            System.out.println(format);
-            String result = Differ.generate(filePath1, filePath2, format);
-            System.out.println(result);
-        } catch(Exception e) {
-            System.out.println("Something wrong");
-            System.err.println(e.getMessage());
+            System.out.println(Differ.generate(filepath1, filepath2));
+        } catch (Exception e) {
             e.printStackTrace();
-            System.err.println(e.getStackTrace().getClass());
             return 1;
         }
         return 0;
     }
+    public static void main(String[] args) throws IOException {
+        App app = CommandLine.populateCommand(new App(), args);
+        if (app.helpRequested) {
+            CommandLine.usage(new App(), System.out);
+        } else if (filepath1 != null && filepath2 != null) {
+            int exitCode = new CommandLine(new App()).execute(args);
+            System.exit(exitCode);
+        }
+
+
+    }
+
 }
--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,36 +1,52 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.TreeMap;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.io.IOException;
-import java.nio.file.*;
-import java.lang.Throwable;
+
+
+
+
 public class Differ {
-    public static String generate(String filePath1, String filePath2) throws Exception {
-        return generate(filePath1, filePath2, "stylish");
+    public static String generate(String filepath1, String filepath2) throws IOException {
+        TypeReference<HashMap<String, Object>> type = new TypeReference<>() { };
+        Path file1 = Paths.get(filepath1).toAbsolutePath();
+        Path file2 = Paths.get(filepath2).toAbsolutePath();
+
+        ObjectMapper mapper = new ObjectMapper();
+        final Map<String, Object> before = mapper.readValue(Files.readString(file1), type);
+        final Map<String, Object> after = mapper.readValue(Files.readString(file2), type);
+
+        Map<String, Object> mixed = new TreeMap<>();
+        mixed.putAll(before);
+        mixed.putAll(after);
+
+        StringBuilder result = new StringBuilder();
+        result.append("{ \n");
+        mixed.keySet()
+                .forEach(i -> {
+                    if (!after.containsKey(i)) {
+                        result.append(" - " + i + ": " + before.get(i) + "\n");
+                    } else if (after.containsKey(i) && before.containsKey(i)) {
+                        if (after.get(i).equals(before.get(i))) {
+                            result.append("   " + i + ": " + after.get(i) + "\n");
+                        } else {
+                            result.append(" - " + i + ": " + before.get(i) + "\n");
+                            result.append(" + " + i + ": " + after.get(i) + "\n");
+                        }
+                    } else if (!before.containsKey(i)) {
+                        result.append(" + " + i + ": " + after.get(i) + "\n");
+                    }
+                });
+        result.append("}");
+
+        return result.toString();
     }
-
-    public static String generate(String filePath1, String filePath2, String format) throws Exception {
-        Path firstFilePath = createPath(filePath1);
-        Path secondFilePath = createPath(filePath2);
-        String contentFile1 = Files.readString(firstFilePath);
-        String contentFile2 = Files.readString(secondFilePath);
-
-        Parser.parse(contentFile1);
-        System.out.println(contentFile1);
-        return contentFile1;
-    }
-
-    private static Path createPath(String pathString) throws Exception {
-        try {
-            return Paths.get(pathString);
-        } catch (InvalidPathException e) {
-            System.out.println("wrong!");
-            String pathMessage = e.getMessage();
-            if(pathMessage == null) {
-                pathMessage = e.toString();
-                System.out.println("Это pathMessage - " + pathMessage);
-            }
-            throw new Exception("Path " + pathString + " is wrong!");
-        }
-    }
-
 }
